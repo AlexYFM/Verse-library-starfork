33a9e7041dd30e73c9f98df54bb87fc503448a00
kbraught <kbraught@iastate.edu>
2024-03-26 10:00:49 -0500

    WIP

--- demo/dryvr_demo/gearbox_demo.py
+++ demo/dryvr_demo/gearbox_demo.py
@@ -12,23 +12,35 @@ class AgentMode(Enum):
 
 
 if __name__ == "__main__":
-    input_code_name = "./demo/dryvr_demo/sleeve_controller2.py"
+
+
+    from verse.scenario import Scenario, ScenarioConfig
+    from verse.analysis import ReachabilityMethod
+    from verse.starsproto.starset import StarSet
+    import polytope as pc
+
+    input_code_name = "./demo/dryvr_demo/sleeve_controller.py"
     config = ScenarioConfig(init_seg_length=1)
-    scenario = Scenario(config=config)
+    scenario = Scenario(config=config, )
+
+    scenario.config.reachability_method = ReachabilityMethod.STAR_SETS
+
 
     car = sleeve_agent("sleeve", file_name=input_code_name)
+    initial_set_polytope_1 = pc.box2poly([[-0.0168,-0.0166], [0.0029,0.0031], [0,0], [0,0], [0,0]])
+
+    car.set_initial(StarSet.from_polytope(initial_set_polytope_1), tuple([AgentMode.Free]))
     scenario.add_agent(car)
 
-    scenario.set_init(
-        [
-            [[-0.0168, 0.0029, 0, 0, 0], [-0.0166, 0.0031, 0, 0, 0]],
-        ],
-        [
-            tuple([AgentMode.Free]),
-        ],
-    )
     traces = scenario.verify(0.2, 0.00001)
-    traces.dump("./demo/gearbox/output.json")
-    fig = go.Figure()
-    fig = reachtube_tree(traces, None, fig, 1, 2, [1, 2, 3, 4, 5], "lines", "trace", sample_rate=1)
-    fig.show()
+
+    
+    import plotly.graph_objects as go
+    from verse.plotter.plotterStar import *
+
+    plot_reachtube_stars(traces, None, 0 , 1)
+
+    #traces.dump("./demo/gearbox/output.json")
+    #fig = go.Figure()
+    #fig = reachtube_tree(traces, None, fig, 1, 2, [1, 2, 3, 4, 5], "lines", "trace", sample_rate=1)
+    #fig.show()
