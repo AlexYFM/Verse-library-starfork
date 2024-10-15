037b12291e52fc84eb3a1eb0bbe029131282cf74
Alex Yuan <alexyuan0.ay@gmail.com>
2024-10-14 19:57:19 -0500

    mostly reorganizing stuff

--- verse/stars/plot_results.py
+++ verse/stars/plot_results.py
@@ -1,13 +1,124 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from starset import *
+from scipy.integrate import ode
+from sklearn.decomposition import PCA
+import pandas as pd
+from syn_sim import *
+
+class PostNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(PostNN, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc4 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
+        # self.relu = nn.ReLU()
+        self.relu = nn.LeakyReLU()
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        # x = self.tanh(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc4(x)
+        x = self.relu(x)
+        # x = self.tanh(x)
+        x = self.fc3(x)
+        x = self.relu(x)
+
+        return x
+
+def positional_encoding(time: torch.Tensor, d_model: int) -> torch.Tensor:
+    pe = torch.zeros(time.size(0), d_model)
+    position = time.unsqueeze(1)  # reshape to (batch_size, 1)
+    
+    div_term = torch.exp(torch.arange(0, d_model, 2) * -(torch.log(torch.tensor(10000.0)) / d_model))
+    pe[:, 0::2] = torch.sin(position * div_term)
+    pe[:, 1::2] = torch.cos(position * div_term)
+    
+    return pe
+
+def eval(initial_star: StarSet, model_path: str, sim_test: Callable, T: int = 7, ts: int = 0.05, num_samples: int = 250) -> None: 
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+
+    # S_0 = sample_star(initial_star, num_samples*10) ### this is critical step -- this needs to be recomputed per training step
+    S = sample_star(initial, num_samples)
+    post_points = []
+    for point in S:
+            post_points.append(sim_test(None, point, T, ts).tolist())
+    post_points = np.array(post_points) ### this has shape N x (T/ts) x (n+1), S_t is equivalent to p_p[:, t, 1:]
+
+    test_times = torch.arange(0, T+ts, ts)
+    pos = positional_encoding(test_times, d_model)
+    test = torch.reshape(test_times, (len(test_times), 1))
+    centers = [] ### eventually this should be a NN output too
+    for i in range(len(test_times)):
+        points = post_points[:, i, 1:]
+        new_center = np.mean(points, axis=0) # probably won't be used, delete if unused in final product
+        centers.append(torch.tensor(new_center, dtype=torch.float))
+
+    post_points = torch.tensor(post_points).float()
+
+    stars = []
+    percent_contained = []
+    cont = lambda p, i: torch.linalg.vector_norm(torch.relu(C@torch.linalg.inv(basis + 1e-6*torch.eye(n))@(p-centers[i])-g)) ### pinv because no longer guaranteed to be non-singular
+    bases = []
+    C, g = initial_star.C, initial_star.g
+    C = torch.tensor(C, dtype=torch.float)
+    g = torch.tensor(g, dtype=torch.float)
+    for i in range(len(test_times)):
+        flat_bases = model(torch.cat((pos[i], torch.tensor(initial_star.basis, dtype=torch.float).flatten()), dim=-1))
+        n = int(len(flat_bases) ** 0.5) 
+        basis = flat_bases.view(-1, n, n)[0]
+        stars.append(StarSet(centers[i], basis.detach().numpy(), C.numpy(), g.numpy()))
+        points = torch.tensor(post_points[:, i, 1:]).float()
+        contain = torch.sum(torch.stack([cont(point, i) == 0 for point in points]))
+        percent_contained.append(contain/(num_samples)*100)
+        size_loss = torch.sqrt(torch.sum(torch.norm(basis, dim=1)))
+        bases.append(size_loss.detach().numpy())
+
+    percent_contained = np.array(percent_contained)
+    plot_stars_points_nonit(stars, post_points)
+    plt.show()
+
+    results = pd.DataFrame({
+        'time': test.squeeze().numpy(),
+        'basis': np.array(bases),
+        'percent of points contained': percent_contained
+    })
+
+    results.to_csv('./verse/stars/nn_results.csv', index=False)
+
+C = np.transpose(np.array([[1,-1,0,0],[0,0,1,-1]]))
+g = np.array([1,1,1,1])
+basis = np.array([[1, 0], [0, 1]]) * np.diag([.1, .1])
+center = np.array([1.50,2.40])
+initial = StarSet(center, basis, C, g)
+d_model = 2*initial.dimension() 
+
+initial_star = StarSet(center, basis, C, g)
+
+
+input_size = basis.flatten().size+d_model 
+hidden_size = 64     # Number of neurons in the hidden layers -- this may change, I know NeuReach has this at default 64
+output_size = basis.flatten().size
+
+model = PostNN(input_size, hidden_size, output_size)
+eval(initial_star, "./verse/stars/model_weights_org_1.pth", sim_test)
 
 results = pd.read_csv('./verse/stars/nn_results.csv').to_numpy()
 plt.plot(results[:, 0], results[:, 1]/max(results[:,1]), label='Size Loss')
 plt.plot(results[:, 0], results[:, 2]/max(results[:,2]), label='Percent Containment')
-# plt.plot(results[:, 0], results[:, 1]/max(results[:,1]), label='Percent Containment')
 plt.xlabel('Time (s)')
-# plt.ylabel(f'Normalized based on {max(results[:,1])}%')
 plt.ylabel(f'Normalized based on {max(results[:,2])}% and {max(results[:,1])}')
 plt.legend()
 plt.show()
--- verse/stars/star_nn.py
+++ verse/stars/star_nn.py
@@ -7,6 +7,7 @@ from scipy.integrate import ode
 from sklearn.decomposition import PCA
 import pandas as pd
 from syn_sim import *
+from tqdm import tqdm
 
 class PostNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
@@ -112,14 +113,13 @@ scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
 
 num_epochs = 30 # sample number of epoch -- can play with this/set this as a hyperparameter
 num_samples = 100 # number of samples per time step
-lamb = 0.5
+lamb = 1.5
 Ns = 10
 
 T = 7
 ts = 0.05
 
 initial_star = StarSet(center, basis, C, g)
-# Toy Function to learn: x^2+20
 
 times = torch.arange(0, T+ts, ts) # times to supply, right now this is fixed while S_t is random. can consider making this random as well
 pos = positional_encoding(times, d_model)
@@ -131,14 +131,9 @@ g = torch.tensor(g, dtype=torch.float)
 S_0 = sample_star(initial_star, num_samples*10) # should eventually be a hyperparameter as the second input, 
 np.random.seed()
 
-def sample_initial(num_samples: int = num_samples) -> List[List[float]]:
-    sample_indices = torch.randint(0, len(S_0), (num_samples,))
-    S = torch.tensor(S_0, dtype=float)
-    return S[sample_indices].tolist()
-
-for epoch in range(num_epochs):
+for epoch in tqdm(range(num_epochs), desc="Training Progress"):
     # Zero the parameter gradients
-    X0 = sample_initial_set(np.array([-3, -3]), np.array([3, 3]), 0.1, Ns)
+    X0 = sample_initial_set(np.array([1, 2]), np.array([2, 3]), 0.25, Ns)
 
     for initial in range(Ns):
         Xi: StarSet = X0[initial]
@@ -170,7 +165,7 @@ for epoch in range(num_epochs):
             r_basis = basis + 1e-6*torch.eye(n) # so that basis should always be inver
             cont = lambda p, i: torch.linalg.vector_norm(torch.relu(C@torch.linalg.inv(r_basis)@(p-centers[i])-g)) ### pinv because no longer guaranteed to be non-singular
             cont_loss = torch.sum(torch.stack([cont(point, i) for point in post_points[:, int(samples_times[i]//ts), 1:]]))/num_samples 
-            size_loss = -torch.log(torch.linalg.det(r_basis@r_basis.mT))
+            size_loss = torch.sqrt(torch.sum(torch.norm(basis, dim=1)))
             loss = lamb*cont_loss + size_loss
             loss.backward()
             optimizer.step()
@@ -186,59 +181,10 @@ for epoch in range(num_epochs):
             r_basis = basis + 1e-6*torch.eye(n) 
             cont = lambda p, i: torch.linalg.vector_norm(torch.relu(C@torch.linalg.inv(r_basis)@(p-centers[i])-g)) ### pinv because no longer guaranteed to be non-singular
             cont_loss = torch.sum(torch.stack([cont(point, i) for point in post_points[:, int(samples_times[i]//ts), 1:]]))/num_samples 
-            size_loss = -torch.log(torch.linalg.det(r_basis@r_basis.mT))
+            size_loss = torch.sqrt(torch.sum(torch.norm(basis, dim=1)))
             loss = lamb*cont_loss + size_loss
             print(f'containment loss: {cont_loss.item():.4f}, size loss: {size_loss.item():.4f}, time: {i*ts:.1f}')
 
 
-# test the new model
-
-model.eval()
-torch.save(model.state_dict(), "./verse/stars/model_weights_org.pth")
-
-# S_0 = sample_star(initial_star, num_samples*10) ### this is critical step -- this needs to be recomputed per training step
-S = sample_initial(num_samples*10)
-post_points = []
-for point in S:
-        post_points.append(sim_test(None, point, T, ts).tolist())
-post_points = np.array(post_points) ### this has shape N x (T/ts) x (n+1), S_t is equivalent to p_p[:, t, 1:]
-
-test_times = torch.arange(0, T+ts, ts)
-test = torch.reshape(test_times, (len(test_times), 1))
-centers = [] ### eventually this should be a NN output too
-for i in range(len(times)):
-    points = post_points[:, i, 1:]
-    new_center = np.mean(points, axis=0) # probably won't be used, delete if unused in final product
-    centers.append(torch.tensor(new_center, dtype=torch.float))
-
-post_points = torch.tensor(post_points).float()
-
-stars = []
-percent_contained = []
-cont = lambda p, i: torch.linalg.vector_norm(torch.relu(C@torch.linalg.inv(basis + 1e-6*torch.eye(n))@(p-centers[i])-g)) ### pinv because no longer guaranteed to be non-singular
-bases = []
-for i in range(len(times)):
-    # mu, center = model(test[i])[0].detach().numpy(), model(test[i])[1:].detach().numpy()
-    flat_bases = model(torch.cat((pos[i], torch.tensor(initial_star.basis, dtype=torch.float).flatten()), dim=-1))
-    n = int(len(flat_bases) ** 0.5) 
-    basis = flat_bases.view(-1, n, n)[0]
-    stars.append(StarSet(centers[i], basis.detach().numpy(), C.numpy(), g.numpy()))
-    points = torch.tensor(post_points[:, i, 1:]).float()
-    contain = torch.sum(torch.stack([cont(point, i) == 0 for point in points]))
-    percent_contained.append(contain/(num_samples*10)*100)
-    size_loss = -torch.log(torch.linalg.det(r_basis@r_basis.mT))
-    bases.append(size_loss.detach().numpy())
-    # stars.append(StarSet(center, bases[i], C.numpy(), mu*g.numpy()))
-    # stars.append(StarSet(centers[i], bases[i], C.numpy(), np.diag(model(test[i]).detach().numpy())@g.numpy()))
-
-percent_contained = np.array(percent_contained)
-plot_stars_points_nonit(stars, post_points)
-plt.show()
-
-results = pd.DataFrame({
-    'time': test.squeeze().numpy(),
-    'basis': np.array(bases),
-    'percent of points contained': percent_contained
-})
-
-results.to_csv('./verse/stars/nn_results.csv', index=False)
+# test the new model - this should be its own file/function
+torch.save(model.state_dict(), "./verse/stars/model_weights_vdp.pth")
