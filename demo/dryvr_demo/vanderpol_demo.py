fixed accuracy computation

demo/dryvr_demo/vanderpol_demo.py
@@ -24,23 +24,33 @@ def plot_stars(stars: List[StarSet], dim1: int = None, dim2: int = None):
 if __name__ == "__main__":
     input_code_name = "./demo/dryvr_demo/vanderpol_controller.py"
     scenario = Scenario(ScenarioConfig(parallel=False))
+    scenario.config.model_path = 'vanderpol_sll'
+
+    scenario.config.model_hparams = {
+        "big_initial_set": (np.array([0,-0.5,0,0,0,0]), np.array([15,0.5,0,0,0,0])), # irrelevant for now
+        "initial_set_size": 1,
+        "lamb": 7,
+        "num_epochs": 30,
+        "gamma":0.99,
+        "lr":1e-4,
+        "sublin_loss":True,
+        # "num_samples": 100,
+        # "Ns": 1
+    }
 
     car = vanderpol_agent("car1", file_name=input_code_name)
     # car = vanderpol_agent('car2', file_name=input_code_name)
     # scenario.add_agent(car)
     # scenario.set_sensor(FakeSensor2())
     # modify mode list input
-    basis = np.array([[1, 0], [0, 1]]) * np.diag([0.01, 0.01])
+    basis = np.array([[1, 0], [0, 1]]) * np.diag([0.1, 0.1])
     center = np.array([1.40,2.30])
     C = np.transpose(np.array([[1,-1,0,0],[0,0,1,-1]]))
     g = np.array([1,1,1,1])
 
-    basis = basis * 100
-    initial = StarSet(center, basis, C, g)
-    initial_trans = StarSet(center-np.array([-1, 1]), basis@np.array([[-1, 1], [1, 0]]), np.transpose(np.array([[1,-1,0,0],[0,0,1,-1]])), g)
-    plot_stars([initial, initial_trans])
+    # initial_trans = StarSet(center-np.array([-1, 1]), basis@np.array([[-1, 1], [1, 0]]), np.transpose(np.array([[1,-1,0,0],[0,0,1,-1]])), g)
+    # plot_stars([initial, initial_trans])
     ### how do I instantiate a scenario with a starset instead of a hyperrectangle?
-    exit() 
     car.set_initial(
             # [[1.25, 2.25], [1.25, 2.25]],
             # [[1.55, 2.35], [1.55, 2.35]]

demo/fp_demos/power_train_star.py
@@ -20,8 +20,8 @@ import plotly.graph_objects as go
 from verse.utils.fixed_points import *
 
 from verse.stars.starset import *
-from verse.sensor.base_sensor_stars import *
 
+from verse.sensor.base_sensor_stars import *
 from verse.analysis.verifier import ReachabilityMethod
 
 class PowerTrainAgent(BaseAgent):
@@ -111,7 +111,19 @@ if __name__ == "__main__":
     PT = PowerTrainAgent('PT', file_name=input_code_name)
 
     scenario = Scenario(ScenarioConfig(init_seg_length=1, parallel=False))
+    scenario.config.model_path = 'powertrain_sublin'
 
+    scenario.config.model_hparams = {
+        "big_initial_set": (np.array([0,-0.5,0,0,0,0]), np.array([15,0.5,0,0,0,0])), # irrelevant for now
+        "initial_set_size": 1,
+        "lamb": 7,
+        "num_epochs": 30,
+        "gamma":0.99,
+        "lr":1e-4,
+        "sublin_loss":True,
+        # "num_samples": 100,
+        # "Ns": 1
+    }
     basis = np.array([[0.001, 0, 0, 0,0], [0, 0.001, 0, 0,0], [0, 0, 0.001, 0,0], [0, 0, 0, 0.001,0], [0,0,0,0,0]])  
     center = np.array([0.6353,14.7, 0.5573, 0.017,0])
     C = np.transpose(np.array([[1,-1,0,0, 0, 0, 0, 0,0,0],[0,0,1,-1, 0, 0, 0,0,0,0], [0,0,0,0,1,-1, 0, 0,0,0],[0,0,0,0,0,0,1,-1,0,0],[0,0,0,0,0,0,0,0,1,-1]]))
@@ -136,18 +148,6 @@ if __name__ == "__main__":
     #     'PT', init_PT,(PTMode.Mode0,)
     # )
 
-    trace = scenario.verify(15, 0.1)
-
-    # pp_fix(reach_at_fix(trace, 0, 15))
-
-    # T, t_step = 15, 0.001
-    # # r_final = reach_at_fix(trace)
-    # # r_candid = reach_at_fix(trace, 0, T-t_step+t_step*.01)
-    # # print(f'Does the candidate reachset contain the final reachset: {contain_all_fix(r_final, r_candid)}')
-    # print(f'Fixed points exists? {fixed_points_fix(trace, 15, 0.001)}')
-
-    # fig = go.Figure()
-    # fig = reachtube_tree(trace, None, fig, 0, 2, [0, 2], "fill", "trace")
-    # # fig = reachtube_tree_slice(trace, None, fig, 0, 2, [0, 2], "fill", "trace", plot_color=colors[1:])
-    # # fig = simulation_tree(trace, None, fig, 1, 2, [1, 2], "fill", "trace")
-    # fig.show()
+    trace = scenario.verify(9.4, 0.1)
+    plot_stars_time(trace, 2)
+    plt.show()

verse/stars/star_nn.py
@@ -191,11 +191,11 @@ for epoch in tqdm(range(num_epochs), desc="Training Progress"):
             # Compute the loss
             r_basis = basis + 1e-6*torch.eye(n) # so that basis should always be inver
             cont = lambda p, i: torch.linalg.vector_norm(torch.relu(C@torch.linalg.inv(r_basis)@(p-centers[i])-g)) ### pinv because no longer guaranteed to be non-singular
-            _, eigenvalues, _ = torch.pca_lowrank(post_points[:, int(samples_times[i]//ts), 1:]) 
+            # _, eigenvalues, _ = torch.pca_lowrank(post_points[:, int(samples_times[i]//ts), 1:]) 
             cont_loss = torch.sum(torch.stack([cont(point, i) for point in post_points[:, int(samples_times[i]//ts), 1:]]))/num_samples 
             size_loss = torch.sqrt(torch.sum(torch.norm(basis, dim=1)))
-            fit_loss = torch.abs(torch.sum(torch.norm(basis, dim=1))-torch.sum(torch.sqrt(eigenvalues[:n])))
-            loss = lamb*cont_loss + size_loss + lamb_fit*fit_loss
+            # fit_loss = torch.abs(torch.sum(torch.norm(basis, dim=1))-torch.sum(torch.sqrt(eigenvalues[:n])))
+            loss = lamb*torch.sqrt(cont_loss) + size_loss
             loss.backward()
             optimizer.step()
         
@@ -212,7 +212,7 @@ for epoch in tqdm(range(num_epochs), desc="Training Progress"):
             cont = lambda p, i: torch.linalg.vector_norm(torch.relu(C@torch.linalg.inv(r_basis)@(p-centers[i])-g)) ### pinv because no longer guaranteed to be non-singular
             cont_loss = torch.sum(torch.stack([cont(point, i) for point in post_points[:, int(samples_times[i]//ts), 1:]]))/num_samples 
             size_loss = torch.sqrt(torch.sum(torch.norm(basis, dim=1)))
-            _, eigenvalues, _ = torch.pca_lowrank(post_points[:, int(samples_times[i]//ts), 1:]) 
+            # _, eigenvalues, _ = torch.pca_lowrank(post_points[:, int(samples_times[i]//ts), 1:]) 
             loss = lamb*cont_loss + size_loss
             print(f'containment loss: {cont_loss.item():.4f}, size loss: {size_loss.item():.4f}, time: {i*ts:.1f}')
 

verse/stars/starset.py
@@ -609,7 +609,7 @@ class StarSet:
         return StarSet(center, basis, C, g)
 
     def print(self) -> None:
-        print(f'Center: {self.center}\n------\nBasis: {self.basis}\n------\C: {self.C}\n------\ng: {self.g}')
+        print(f'Center: {self.center}\n------\nBasis: {self.basis}\n------\nC: {self.C}\n------\ng: {self.g}')
 
 class HalfSpace:
     '''
@@ -1121,7 +1121,7 @@ def get_model(initial: StarSet, sim: Callable, mode_label: int = None, T: float 
 def gen_reachtube(initial: StarSet, sim: Callable, model: PostNN, mode_label: int = None, num_samples: int=100, T: float = 7, ts: float = 0.05, lane_map: LaneMap=None, verbose: bool = False) -> List[StarSet]:
     # if verbose:
     #     print(f'In mode {mode_label}')
-    #     initial.print()
+        # initial.print()
     
     S = sample_star(initial, num_samples)
     post_points = []
@@ -1144,28 +1144,31 @@ def gen_reachtube(initial: StarSet, sim: Callable, model: PostNN, mode_label: in
         flat_bases: torch.Tensor = model(torch.cat((x0, torch.tensor(initial.basis, dtype=torch.float).flatten(), test_times[i].unsqueeze(0)), dim=-1))
         n = int(len(flat_bases) ** 0.5) 
         basis = flat_bases.view(-1, n, n)[0]
-        stars.append(StarSet(center, basis.detach().numpy(), C, g))    
+        new_star = StarSet(center, basis.detach().numpy(), C, g)
+        stars.append(new_star)    
 
         if verbose:
-            accuracy.append(compute_accuracy(initial, points, basis))
+            accuracy.append(compute_accuracy(new_star, points, basis))
             # if (i+1)%(10) == 0:
             #     print(f'Accuracy {accuracy[-1]} at t={test_times[i]}')     
-        plt.scatter(np.ones(len(points[:,0]))*i*ts, points[:,0]) # just for plotting
+        # plt.scatter(np.ones(len(points[:,0]))*i*ts, points[:,2]) # just for plotting
+        plt.scatter(points[:,0], points[:,1]) # just for plotting
 
     accuracy = np.array(accuracy)
 
     if verbose:
         print(f'Average accuracy: {np.mean(accuracy):.2f}., Worst Accuracy: {np.min(accuracy):.2f}') # this will get printed out for each node -- to not have this behavior, just store it in some global
-    
     # plot_stars_points_nonit_nd(stars, post_points, 0, 2)
     return stars
 
 def compute_accuracy(initial: StarSet, points: np.ndarray, new_basis: torch.Tensor):
     n = initial.n
     new_center = np.mean(points, axis=0)
-    r_basis = 1e-6+np.eye(n)+new_basis.detach().numpy()
+    r_basis = 1e-6*np.eye(n)+new_basis.detach().numpy()
     cont = lambda p: np.linalg.norm(np.maximum(initial.C@np.linalg.inv(r_basis)@(p-new_center)-initial.g, 0))
     contain = np.sum(np.stack([cont(point) == 0 for point in points]))
+    # for point in points:
+    #     print(cont(point))
     return np.round(contain/len(points), 3)
 
 def write_train_details(model_path: str, num_epochs: int = 30, num_samples: int = 100, 